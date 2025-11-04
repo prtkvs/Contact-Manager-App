30716a7b22d6429378f83a1db842bf337d0166b0
Prateek Verma <prateek222rithik@gmail.com>
2025-11-04 11:56:56 +0530

    cors defined

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -10,7 +10,22 @@ const port = process.env.PORT || 5000;
 // app.get("/api/contacts",(req,res)=>{
 //     res.send("Get all contacts");
 // });
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://contact-manager-app-flame.vercel.app"
+];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true
+  })
+);
+
+//testing api
+app.get("/api", (req, res) => {
+  res.send("API is running");
+});
+
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
